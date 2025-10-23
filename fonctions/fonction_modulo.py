ee3a07260b87e79b6fc1106892aa9765b01157bf
Lulucav2 <delhaye.axelv2@gmail.com>
2025-10-23 09:56:27 +0200

    ajout fonction multi modulo

--- fonctions/fonction_modulo.py
+++ fonctions/fonction_modulo.py
@@ -1,3 +1,11 @@
-def div(a,b):
-    a % b
-    return(a)
+def modulo(a,b):
+    """Retourne le modulot de tous les arguments fournis.
+
+    Args:
+        args: Nombres à diviser.
+
+    Returns:
+        Le produit des nombres.
+    """
+    return a % b 
+    
--- fonctions/fonction_multi.py
+++ fonctions/fonction_multi.py
@@ -1,3 +1,13 @@
-def div(a,b):
-    a * b
-    return(a)
+def multi(*args):
+    """Retourne le produit de tous les arguments fournis.
+
+    Args:
+        args: Nombres à multiplier.
+
+    Returns:
+        Le produit des nombres.
+    """
+    produit = 1
+    for nombre in args:
+        produit *= nombre
+    return produit
